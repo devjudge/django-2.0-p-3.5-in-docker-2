ce0362e22fe4f0cd82942e84191e66a74610b133
Simar Singh <simar@codejudge.io>
2020-07-26 19:28:22 +0530

    set default database to sqlite | pick mysql and mongodb properties from config.ini

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read(os.path.join(BASE_DIR, 'config.ini'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
